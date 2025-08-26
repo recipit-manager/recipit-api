769cd37e5f287a0da07f253e21a0c43942a8de84
최우빈 <3jdnqls@naver.com>
2025-08-26 16:21:15 +0900

    AC06 국가코드 목록 조회 수정

--- src/main/java/toy/recipit/service/UserService.java
+++ src/main/java/toy/recipit/service/UserService.java
@@ -10,7 +10,6 @@ public class UserService {
     private final UserMapper userMapper;
 
     public boolean isNicknameDuplicate(String nickname) {
-
         return userMapper.isNicknameDuplicate(nickname);
     }
 }
