9887f3d23b4eb16d63d9c1ee0581cbe84e0cd629
최우빈 <3jdnqls@naver.com>
2025-09-02 15:52:55 +0900

    인증코드생성 클래스 수정

--- src/main/java/toy/recipit/common/util/EmailVerificationCodeGenerator.java
+++ src/main/java/toy/recipit/common/util/EmailVerificationCodeGenerator.java
@@ -6,7 +6,6 @@ import java.security.SecureRandom;
 
 @Component
 public class EmailVerificationCodeGenerator {
-    private static final int MAX_VERIFICATION_CODE_LENGTH = 32;
     private static final int DEFAULT_VERIFICATION_CODE_LENGTH = 8;
     private static final SecureRandom RANDOM = new SecureRandom();
 
@@ -14,21 +13,14 @@ public class EmailVerificationCodeGenerator {
         return createVerificationCode(DEFAULT_VERIFICATION_CODE_LENGTH);
     }
 
-    public String createVerificationCode(int length) {
-        if (length <= 0) {
-            throw new IllegalArgumentException("validation.verification_code.blank");
-        }
-
-        if (length > MAX_VERIFICATION_CODE_LENGTH) {
-            throw new IllegalArgumentException("validation.verification_code.size");
-        }
-
+    private String createVerificationCode(int length) {
         StringBuilder sb = new StringBuilder(length);
 
         for (int i = 0; i < length; i++) {
             sb.append(Constants.RandomCode.CHAR_POOL_WITHOUT_SIMILAR
                     .charAt(RANDOM.nextInt(Constants.RandomCode.CHAR_POOL_WITHOUT_SIMILAR.length())));
         }
+
         return sb.toString();
     }
 }
