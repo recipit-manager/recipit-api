5a3834ad9575bcb141ebcaae312255e05f4a0df1
최우빈 <3jdnqls@naver.com>
2025-09-16 17:08:20 +0900

    AC12 비밀번호 변경 1 개발

---
+++ src/main/java/toy/recipit/controller/dto/request/ChangeTemporaryPasswordDto.java
@@ -0,0 +1,25 @@
+package toy.recipit.controller.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ChangeTemporaryPasswordDto {
+    @NotBlank(message = "validation.password.blank")
+    @Size(min = 8, max = 16, message = "validation.password.size")
+    @Pattern.List({
+            @Pattern(
+                    regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^\\w\\s]).{8,16}$",
+                    message = "validation.password.complexity"
+            ),
+            @Pattern(
+                    regexp = "^(?!.*(.)\\1\\1).*$",
+                    message = "validation.password.repetition"
+            )
+    })
+    private final String password;
+}
