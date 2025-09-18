ddcf0c863b194604f6686ad5a22aa83cd3d32647
최우빈 <3jdnqls@naver.com>
2025-09-18 15:35:51 +0900

    오타 수정

--- src/main/java/toy/recipit/common/util/SessionUtil.java
+++ src/main/java/toy/recipit/common/util/SessionUtil.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Component;
 import toy.recipit.common.Constants;
 import toy.recipit.common.exception.NotLoginStatusException;
 import toy.recipit.common.exception.SessionNotExistsException;
-import toy.recipit.common.exception.UserNotFoundException;
 import toy.recipit.controller.dto.response.SessionUserInfo;
-
-import java.util.Optional;
 
 @Component
 public class SessionUtil {
