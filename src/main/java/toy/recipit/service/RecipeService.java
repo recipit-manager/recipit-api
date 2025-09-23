bda3a20c8a10becccaa6ae23c251bd50f6ef3ae6
최우빈 <3jdnqls@naver.com>
2025-09-23 09:48:04 +0900

    RC03 임시저장 레시피 개수 조회 개발

--- src/main/java/toy/recipit/service/RecipeService.java
+++ src/main/java/toy/recipit/service/RecipeService.java
@@ -31,4 +31,8 @@ public class RecipeService {
                 ))
                 .toList();
     }
+
+    public Integer getDraftRecipeCount(String userNo) {
+        return recipeMapper.getRecipeCount(userNo, Constants.Recipe.DRAFT);
+    }
 }
