64d57fbfa9683459319d93405920fb42e934ae47
최우빈 <3jdnqls@naver.com>
2025-09-23 13:41:54 +0900

    RC04-05 레시피 좋아요, 좋아요 취소 개발

--- src/main/java/toy/recipit/service/RecipeService.java
+++ src/main/java/toy/recipit/service/RecipeService.java
@@ -2,6 +2,7 @@ package toy.recipit.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import toy.recipit.common.Constants;
 import toy.recipit.common.util.ImageKitUtil;
 import toy.recipit.controller.dto.response.PopularRecipeDto;
@@ -34,5 +35,31 @@ public class RecipeService {
 
     public int getDraftRecipeCount(String userNo) {
         return recipeMapper.getRecipeCount(userNo, Constants.Recipe.DRAFT);
+    }
+
+    @Transactional
+    public Boolean likeRecipe(String userNo, String recipeNo) {
+        if(!recipeMapper.isRecipeExists(recipeNo)) {
+            throw new IllegalArgumentException("recipe.notFoundRecipe");
+        }
+
+        if (recipeMapper.isLikeExists(userNo, recipeNo)) {
+            recipeMapper.updateLike(userNo, recipeNo, Constants.Yn.YES);
+        } else {
+            recipeMapper.insertLike(userNo, recipeNo, Constants.Yn.YES);
+        }
+
+        return true;
+    }
+
+    @Transactional
+    public Boolean unlikeRecipe(String userNo, String recipeNo) {
+        if(!recipeMapper.isRecipeExists(recipeNo)) {
+            throw new IllegalArgumentException("recipe.notFoundRecipe");
+        }
+
+        recipeMapper.updateLike(userNo, recipeNo, Constants.Yn.NO);
+
+        return true;
     }
 }
