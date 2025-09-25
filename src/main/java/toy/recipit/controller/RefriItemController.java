1cc902af8105318e31e23e36b3603f7945686a29
최우빈 <3jdnqls@naver.com>
2025-09-25 14:38:27 +0900

    예외 핸들러 추가, 자동완성 requestDTO삭제

--- src/main/java/toy/recipit/controller/RefriItemController.java
+++ src/main/java/toy/recipit/controller/RefriItemController.java
@@ -1,13 +1,13 @@
 package toy.recipit.controller;
 
-import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import toy.recipit.controller.dto.request.AutoCompleteDto;
 import toy.recipit.controller.dto.response.ApiResponse;
 import toy.recipit.controller.dto.response.factory.ApiResponseFactory;
 import toy.recipit.service.RefriItemService;
@@ -17,14 +17,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/refri-item")
 @RequiredArgsConstructor
+@Validated
 public class RefriItemController {
     private final ApiResponseFactory apiResponseFactory;
     private final RefriItemService refriItemService;
 
     @GetMapping("/ingredient/auto-complete")
     public ResponseEntity<ApiResponse<List<String>>> getAutoCompleteList(
-            @Valid @ModelAttribute AutoCompleteDto autoCompleteDto
+            @RequestParam
+            @NotBlank(message = "refriItem.keyword.blank")
+            String keyword
     ) {
-        return ResponseEntity.ok(apiResponseFactory.success(refriItemService.getAutoCompleteList(autoCompleteDto.getKeyword())));
+        return ResponseEntity.ok(apiResponseFactory.success(refriItemService.getAutoCompleteList(keyword)));
     }
 }
