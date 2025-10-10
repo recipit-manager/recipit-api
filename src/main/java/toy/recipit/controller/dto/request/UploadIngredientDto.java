1a8d7b6e52ec3f9d2560aff0c433a75b2816eb3b
최우빈 <3jdnqls@naver.com>
2025-10-10 09:47:43 +0900

    오타 수정

--- src/main/java/toy/recipit/controller/dto/request/UploadIngredientDto.java
+++ src/main/java/toy/recipit/controller/dto/request/UploadIngredientDto.java
@@ -6,8 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import toy.recipit.common.validation.Draft;
-import toy.recipit.common.validation.Upload;
 
 @Getter
 @RequiredArgsConstructor
