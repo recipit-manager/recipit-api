2571f217c6b4b4ddf874585e27099c4a8dfa8410
wjdwo951219 <wjdwo951219@gmail.com>
2025-11-28 11:10:15 +0900

    apply domain

--- src/main/java/toy/recipit/controller/UserController.java
+++ src/main/java/toy/recipit/controller/UserController.java
@@ -297,7 +297,7 @@ public class UserController {
 
     private void setAutoLoginCookie(HttpServletResponse response, String token) {
         ResponseCookie cookie = ResponseCookie.from(Constants.UserLogin.AUTO_LOGIN_COOKIE_NAME, token)
-                .domain(".code.run")
+                .domain(".mockiwi.com")
                 .sameSite("none")
                 .secure(true)
                 .httpOnly(true)
